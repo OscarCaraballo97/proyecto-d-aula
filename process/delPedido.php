3f2f8eaa295d20b91ebd264aad85e8cd47992269
Darkraii97 <Caraballooscardavid@gmail.com>
2024-05-19 23:23:27 -0500

    se agrega eliminacion de pedidos

---
+++ process/delPedido.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include '../Consulta/consultaSql.php';
+
+$NumPedidoDel=consultas::clean_string($_POST['num-pedido']);
+if(consultas::DeleteSQL('detalle', "NumPedido='".$NumPedidoDel."'") && consultas::DeleteSQL("venta", "NumPedido='".$NumPedidoDel."'")){
+    echo '<script>
+	    swal({
+	      title: "Pedido eliminado",
+	      text: "El pedido se eliminó con éxito",
+	      type: "success",
+	      showCancelButton: true,
+	      confirmButtonClass: "btn-danger",
+	      confirmButtonText: "Aceptar",
+	      cancelButtonText: "Cancelar",
+	      closeOnConfirm: false,
+	      closeOnCancel: false
+	      },
+	      function(isConfirm) {
+	      if (isConfirm) {
+	        location.reload();
+	      } else {
+	        location.reload();
+	      }
+	    });
+	</script>';
+}else{
+   echo '<script>swal("ERROR", "Ocurrió un error inesperado, por favor intente nuevamente", "error");</script>';
+}
